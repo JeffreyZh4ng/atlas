13d110c00c31b1976c374b6c6d20737cb65aad9a
jeffreyzhang <jeffrey.zh4ng@gmail.com>
2018-12-22 18:23:07 -0600

    Migrated from jade to pug. Created some example routes in index

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//TODO: Need to find out what this users route is for
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//TODO: Set up login page here
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
